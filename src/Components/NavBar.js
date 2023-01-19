95e76e5fc57cd601ada1376056ca30817000f9da
Kyle Brackman <kyle.a.brackman@gmail.com>
2023-01-18 19:06:43 -0700

    added better css to review page

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -8,16 +8,16 @@ import Stack from '@mui/material/Stack';
 function NavBar() {
 
     return (
-        <Stack direction="row" spacing={2} style={{justifyContent: "center", paddingTop: 20}}>
-                <NavLink to="/home">
-                    <Button variant="contained">GEAR HEADZ</Button>
-                </NavLink>
-                <NavLink to="/reviews">
+        <Stack direction="row" spacing={2} style={{ justifyContent: "center", paddingTop: 20 }}>
+            <NavLink to="/home">
+                <Button variant="contained">GEAR HEADZ</Button>
+            </NavLink>
+            <NavLink to="/reviews">
                 <Button variant="contained">Reviews!</Button>
-                </NavLink>
-                <NavLink to="/submissions">
+            </NavLink>
+            <NavLink to="/submissions">
                 <Button variant="contained">Submit a Review!</Button>
-                </NavLink>
+            </NavLink>
         </Stack>
     )
 }
--- src/Components/ReviewPage.js
+++ src/Components/ReviewPage.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-function ReviewPage() {
+
+function ReviewPage({ items }) {
+
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }));
+
+    const itemList = items.map(item => {
+        return (
+            <Box key={item.item_id} sx={{ margin: 4, flexGrow: 1 }}>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
+                    <Grid item xs={6}>
+                        <Item>
+                            {item.item_name}
+                            {item.item_type}
+                            <DeleteIcon></DeleteIcon>
+                        </Item>
+                    </Grid>
+                </Grid>
+            </Box>
+        )
+    })
+
     return (
-        <div>
-            <p>test</p>
-        </div>
+        <Grid >
+            {itemList}
+        </Grid>
     )
 }
 
