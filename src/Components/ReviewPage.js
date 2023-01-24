ccf2766f8562f48654cd23900899a8d9687dcd33
Kyle Brackman <kyle.a.brackman@gmail.com>
2023-01-24 10:33:39 -0700

    managed to center and align item cards correctly finally

--- src/Components/ReviewPage.js
+++ src/Components/ReviewPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import ItemCard from "./ItemCard";
 
 import { Box, Grid } from "@mui/material";
+import { Stack } from "@mui/system";
 
 
 
@@ -27,12 +28,14 @@ function ReviewPage({ items, handleDeleteItems }) {
     })
 
     return (
-        <Box key={items.id} sx={{ paddingTop:10, alignItems:"center", marginLeft: 20 }} >
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ }} >
-                <Grid item xs={4}></Grid>
-                {itemCards}
-            </Grid>
-        </Box >
+        <Stack sx={{alignItems: "center"}}>
+            <Box key={items.id} sx={{ paddingTop: 10, alignItems: "center" }} >
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{}} >
+                    <Grid item xs={4}></Grid>
+                    {itemCards}
+                </Grid>
+            </Box >
+        </Stack>
     )
 }
 
--- src/Components/Submissions.js
+++ src/Components/Submissions.js
@@ -1,13 +1,31 @@
 import React from "react";
 
-import Box from '@mui/material/Box';
-import { FormControl } from "@mui/material";
+import { TextField, Select, MenuItem, FormControl, Button, Box, Stack, Paper } from "@mui/material";
+import { styled } from '@mui/material/styles';
+
 
 function Submissions() {
+
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        width: 1000
+    }));
+
     return (
-        <FormControl>
-                <Box sx={{ bgcolor: '#282c34', height: '100vh' }} />
-        </FormControl>
+        <Stack sx={{ alignItems: "center" }}>
+            <FormControl>
+                <Box>
+                    <Item sx={{ marginTop: 10 }}>
+                        <p style={{ fontSize: 20 }}>Submit a New Item!</p>
+                        <TextField />
+                    </Item>
+                </Box>
+            </FormControl>
+        </Stack>
     )
 }
 
